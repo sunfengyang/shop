9220ff117410b3b132365948acb0c75a43f45795
sunfengyang <363257617@qq.com>
2019-08-01 18:31:32 +0800

    首页

--- src/main.js
+++ src/main.js
@@ -2,9 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+Vue.config.productionTip = false
+
+//引入所有vant插件
+//import Vant from 'vant';
+//import 'vant/lib/index.css';
+//Vue.use(Vant);
+
+//按需引入插件（更为推荐）
+import {NavBar,Icon,Tabbar, TabbarItem,Swipe, SwipeItem,Lazyload} from 'vant';
+Vue.use(NavBar).use(Icon).use(Tabbar).use(TabbarItem).use(Swipe).use(SwipeItem).use(Lazyload);
+
 import '@/assets/css/reset.css'
 import '@/assets/js/rem.js'
-Vue.config.productionTip = false
 
 new Vue({
   router,
