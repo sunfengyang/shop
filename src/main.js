f751935f74d17932debcb11e3272caca8c4431a2
sunfengyang <363257617@qq.com>
2019-09-14 16:01:57 +0800

    注册登录

--- src/main.js
+++ src/main.js
@@ -11,8 +11,8 @@ Vue.config.productionTip = false
 //Vue.use(Vant);
 
 //按需引入插件（更为推荐）
-import {NavBar,Icon,Tabbar, TabbarItem,Swipe, SwipeItem,Lazyload} from 'vant';
-Vue.use(NavBar).use(Icon).use(Tabbar).use(TabbarItem).use(Swipe).use(SwipeItem).use(Lazyload);
+import {NavBar,Icon,Tabbar, TabbarItem,Swipe, SwipeItem,Lazyload,Tab,Tabs,CellGroup,Field} from 'vant';
+Vue.use(NavBar).use(Icon).use(Tabbar).use(TabbarItem).use(Swipe).use(SwipeItem).use(Lazyload).use(Tab).use(Tabs).use(CellGroup).use(Field);
 
 import '@/assets/css/reset.css'
 import '@/assets/js/rem.js'
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,12 +1,58 @@
 <template>
-  <div>我的</div>
+  <div>
+    <van-tabs>
+      <van-tab title="登录">
+        <van-cell-group>
+          <van-field label="用户名" required clearable placeholder="请输入用户名" v-model="loginUsername"></van-field>
+          <van-field label="密码" type="password" required clearable placeholder="请输入密码" v-model="loginPassword"></van-field>
+        </van-cell-group>
+        <div>
+          <van-button type="primary" size="large">登录</van-button>
+        </div>
+      </van-tab>
+      <van-tab title="注册">
+        <van-cell-group>
+          <van-field label="用户名" required clearable placeholder="请输入用户名" v-model="registUsername"></van-field>
+          <van-field label="密码" type="password" required clearable placeholder="请输入密码" v-model="registPassword"></van-field>
+        </van-cell-group>
+        <div>
+          <van-button type="primary" size="large" @click="registHandler">注册</van-button>
+        </div>
+      </van-tab>
+    </van-tabs>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+import url from '@service.config.js';
 
 
 export default {
-  
+  data(){
+    return{
+      loginUsername:'',
+      loginPassword:'',
+      registUsername:'',
+      registPassword:'',
+    }
+  },
+  methods:{
+    registHandler(){
+      axios({
+        url:'url.registUser',
+        method:'post',
+        data:{
+          userName:this.registUsername,
+          password:this.registPassword
+        }
+      }).then(res=>{
+
+      }).catch(err=>{
+
+      });
+    },
+  }
 }
 </script>
 
